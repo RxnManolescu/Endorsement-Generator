b88390cc88d71933a3f02e5adb490189ee862528
Roxana Manolescu <rxn.manolescu@gmail.com>
2024-02-23 22:49:14 +0000

    Added "from" and "to" input fields and rendered them in the endorsement. I also added a black heart img to the output.

--- index.js
+++ index.js
@@ -12,21 +12,32 @@ const endorsementsListInDb = ref(database, "endorsementsList")
 const inputEl = document.getElementById("input-field")
 const publishBtnEl = document.getElementById("publish-btn")
 const endorsementsListEl = document.getElementById("endorsements-list")
+const fromEl = document.getElementById("from-btn")
+const toEl = document.getElementById("to-btn")
 
 publishBtnEl.addEventListener("click", function() {
     let inputValue = inputEl.value
-    appendEndorsementToList(inputValue)
+    let fromValue = fromEl.value
+    let toValue = toEl.value
+    appendEndorsementToList(inputValue, fromValue, toValue)
     push(endorsementsListInDb, inputValue)
     inputEl.value = ""
+    fromEl.value = ""
+    toEl.value = ""
 })
 
-function appendEndorsementToList(userInput) {
+function appendEndorsementToList(userInput, from, to) {
     // let userInputID = userInput[0]
     // let userInputValue = userInput[1]
 
     let newEndorsementEl = document.createElement("p")
     newEndorsementEl.className = "endorsement"
-    newEndorsementEl.innerHTML = userInput
 
+    newEndorsementEl.innerHTML = `<strong>To ${to}</strong>\n\n
+                                  <div style="margin: 10px 0;">${userInput}</div>\n
+                                  <div style="display: flex; align-items: center;">
+                                  <strong>From ${from}</strong>
+                                  <span style="margin-left: auto;"><img src="./assets/like-heart.png"></span>
+                                  </div>`;
     endorsementsListEl.append(newEndorsementEl)
 }
